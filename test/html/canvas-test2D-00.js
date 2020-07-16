9d928200d6a96188d9672b97890d4fcc48174f54
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2020-07-16 15:58:45 +0200

    test2D-00 is coming

--- test/html/canvas-test2D-00.js
+++ test/html/canvas-test2D-00.js
@@ -3,7 +3,8 @@ import testBed from './testBed.js'
 
 const consoleLog = testBed.subscribe('#f09')
 
-//
+
+
 class MyNode extends flex.Node {
 
     static repeat(n, layout) {
@@ -16,13 +17,52 @@ class MyNode extends flex.Node {
 
     static new(layout) { return new MyNode(layout) }
 
+    static horizontal(layout) { return new MyNode({ ...layout, flexDirection:'horizontal' }) }
+    static vertical(layout) { return new MyNode({ ...layout, flexDirection:'vertical' }) }
+
     constructor(layout) {
 
         super()
 
         this.layout = layout
     }
+
+    setLayout(layout) {
+
+        Object.assign(this.layout, layout)
+
+        return this
+    }
+
+    fillHorizontal(layout) { return this.setLayout({ ...layout, width:'1w', height:'100%' }) }
+    fillVertical(layout) { return this.setLayout({ ...layout, width:'100%', height:'1w' }) }
 }
 
-const root = MyNode.new({ width:600, height:300, gutter:10, padding:10 }).add(
+const root = MyNode.horizontal({ width:600, height:300, gutter:10, padding:10 }).add(
+    MyNode.horizontal().add(
+        MyNode.horizontal().add(
+            MyNode.horizontal(),
+            MyNode.horizontal(),
+            MyNode.vertical().add(
+                MyNode.vertical(),
+                MyNode.vertical(),
+                MyNode.horizontal(),
+            ),
+        ),
+    ),
+    MyNode.horizontal(),
+    MyNode.vertical().add(
+        MyNode.horizontal(),
+        MyNode.vertical().add(
+            MyNode.vertical(),
+            MyNode.vertical(),
+            MyNode.vertical(),
+        ),
+    ),
+    MyNode.vertical(),
 )
+
+Object.assign(globalThis, { root })
+
+for (const node of root.flat({ includeSelf:false }))
+    node.parent.layout.flexDirection === 'vertical' ? node.fillVertical() : node.fillHorizontal()
