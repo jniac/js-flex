65f32a0e6c233c983818ac8bcad6fa7864d87489
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2020-08-05 19:46:54 +0200

    update 2D tests

--- test/html/2D/MyNode.js
+++ test/html/2D/MyNode.js
@@ -21,8 +21,7 @@ export default class MyNode extends flex.Node {
 
         this.layout = {}
 
-        if (layout)
-            this.setLayout(layout)
+        this.setLayout({ width:'fill', height:'fill', ...layout})
     }
 
     setLayout(layout) {
--- test/html/2D/test-0x.js
+++ test/html/2D/test-0x.js
@@ -5,13 +5,35 @@ import MyNode from './MyNode.js'
 
 const consoleLog = testBed.subscribe('#f09')
 
-const root = MyNode.vertical({ width:500, height:200, gutter:4, padding:10 }).add(
-    MyNode.vertical({ width:'40%', height:'2w' }),
-    MyNode.vertical({ width:'40%', height:'1w' }),
-    MyNode.vertical({ width:'40%', height:'1w' }),
+const root = MyNode.horizontal({ width:400, height:200, gutter:10, padding:10 }).add(
+    MyNode.horizontal().add(
+        MyNode.horizontal().add(
+            MyNode.horizontal(),
+            MyNode.horizontal(),
+            MyNode.vertical().add(
+                MyNode.vertical(),
+                MyNode.vertical(),
+                MyNode.horizontal(),
+            ),
+        ),
+    ),
+    MyNode.horizontal(),
+    MyNode.vertical().add(
+        MyNode.horizontal(),
+        MyNode.vertical().add(
+            MyNode.vertical(),
+            MyNode.vertical(),
+            MyNode.vertical(),
+        ),
+    ),
+    MyNode.vertical(),
 )
 
 Object.assign(globalThis, { root })
+
+for (const node of root.flat({ includeSelf:false }))
+    // node.fill()
+    node.setLayout({ width:'100%', height:'20%' })
 
 const { rootNode } = flex.compute2D(root, { verbose:consoleLog })
 
