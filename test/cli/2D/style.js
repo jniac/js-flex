49a48537be0686221d5dd6cc1813973e7a5e7ef7
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2020-08-29 19:01:41 +0200

    update test/cli

--- test/cli/2D/style.js
+++ test/cli/2D/style.js
@@ -2,6 +2,7 @@ const assert = require('assert')
 const { test, describe, it, almostEquals } = require('../testbed.js')
 
 const flex = require('../../../build/flex.common.js')
+const { Style } = flex
 
 const main = () => {
 
@@ -9,25 +10,25 @@ const main = () => {
 
         it (`'width' triggers 2D style`, () => {
 
-            assert.equal(new flex.Style({ height:300 }).is2D, true)
+            assert.equal(new Style({ height:300 }).is2D, true)
         })
 
         it (`default 'direction' is "horizontal"`, () => {
 
-            assert.equal(new flex.Style({ width:1 }).direction, 'horizontal')
+            assert.equal(new Style({ width:1 }).direction, 'horizontal')
         })
 
         it(`depending on 'direction', 'height' is transfered to normal.size`, () => {
 
-            assert.equal(new flex.Style({ height:Math.PI }).normal.size, Math.PI)
+            assert.equal(new Style({ height:Math.PI }).normal.size, Math.PI)
         })
 
         it(`getter|setter 'width|height' are correctly linked`, () => {
 
             const width = Math.PI
             const height = Math.E
-            const H = new flex.Style({ width, height })
-            const V = new flex.Style({ direction:'vertical', width, height })
+            const H = new Style({ width, height })
+            const V = new Style({ direction:'vertical', width, height })
             assert.equal(H.width, width)
             assert.equal(H.height, height)
             assert.equal(V.width, width)
@@ -41,6 +42,20 @@ const main = () => {
             assert.equal(H.height, 3)
             assert.equal(V.width, 4)
             assert.equal(V.height, 3)
+        })
+
+        it(`Style.keys & Style.splitKeys`, () => {
+
+            const props = { lol:43 }
+
+            for (const key of Style.keys)
+                props[key] = Math.random()
+
+            const [style, rest] = Style.splitKeys(props)
+
+            assert.equal(Object.keys(style).length, Style.keys.length)
+            assert.equal(Object.keys(rest).length, 1)
+            assert.equal(rest.lol, props.lol)
         })
     })
 }
